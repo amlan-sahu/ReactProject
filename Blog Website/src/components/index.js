628ce8e82c97734e901a985d6c6dd065e7f264c1
amlan-sahu <amlan9531@gmail.com>
2024-12-29 01:40:30 +0530

    Fixing typo

--- Blog Website/src/components/index.js
+++ Blog Website/src/components/index.js
@@ -1,10 +1,10 @@
 import Header from "./Header/Header.jsx";
 import Footer from "./Footer/Footer.jsx";
 import Container from "./container/container.jsx";
-import Logo from "./logo.jsx";
+import Logo from "./Logo.jsx";
 import LogoutBtn from "./Header/LogoutBtn.jsx";
 import Button from "./Button.jsx";
-import Input from "./input.jsx";
+import Input from "./Input.jsx";
 
 export{
     Header,
